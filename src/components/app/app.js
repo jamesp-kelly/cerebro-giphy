6a81f82a56966c560240589b1ec4ab9405bfc19c
James Kelly <jk_jameskelly@outlook.com>
2017-04-21 13:56:22 -0400

    debounce search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Bricks from 'bricks.js';
+import { debounce } from 'lodash';
 import { getTrending, search } from '../../api';
 import styles from '../../styles.css';
 import Grid from '../grid/grid';
@@ -10,18 +11,17 @@ export default class App extends Component {
     super(props);
     this.state = {
       gifs: null,
-    }
-  }
-
-  componentDidMount() {
+    };
+    this.debouncedSearch = debounce((searchTerm) => {
+      search(searchTerm).then(results => {
+        this.setState({ gifs: results.data});
+      });
+    }, 1000); 
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchTerm !== this.props.searchTerm) {
-      search(this.props.searchTerm).then(results => {
-        this.setState({ gifs: results.data});
-
-      });
+      this.debouncedSearch(this.props.searchTerm);
     }
   }
 
